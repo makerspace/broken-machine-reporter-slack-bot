f1e95cf3754d529f7ff45baefa77453e2a9e98b0
Erasmus Cedernaes <github@emaus.dev>
2026-06-07 10:31:47 +0200

    Read in config file on startup

--- docker-start.sh
+++ docker-start.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 docker build . --tag broken-machines
-docker run --rm -d broken-machines
+docker run --rm -d \
+  -v "$(pwd)/rooms.yaml:/app/rooms.yaml:ro" \
+  --name broken-machines \
+  broken-machines
